0039905fcad2d4bf8bea0f72a7a969fe62ef46b7
Niall <nialldbarber@gmail.com>
2019-08-10 14:27:35 +0100

    Playing around with events

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,35 @@
 <script>
   import Todo from "./Todo.svelte";
+
+  let todos = [
+    { id: 1, todo: "learn svelte" },
+    { id: 2, todo: "learn react" },
+    { id: 3, todo: "learn vue" }
+  ];
+  let todo = "";
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(todo);
+    todos = [...todos, { id: Math.random(), todo: todo }];
+  };
 </script>
 
 <style lang="scss">
   .todo-container {
-    display: flex;
-    align-items: center;
   }
 </style>
 
 <div class="todo-container">
-  <Todo />
+  <h1>Todos</h1>
+  <form on:submit={handleSubmit}>
+    <label>
+      <input type="text" bind:value={todo} />
+    </label>
+  </form>
+  <ul>
+    {#each todos as { id, todo }}
+      <li>{todo}</li>
+    {/each}
+  </ul>
 </div>
